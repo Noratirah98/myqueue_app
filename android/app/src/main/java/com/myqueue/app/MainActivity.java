e0472ecacae96403dff7fec18fa8ac34bfcbbbbd
Noratirah98 <88771150+Noratirah98@users.noreply.github.com>
2026-02-01 19:31:14 +0800

    Setup for icon and splash app

--- android/app/src/main/java/com/myqueue/app/MainActivity.java
+++ android/app/src/main/java/com/myqueue/app/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.myqueue.app;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'MyQueue',
+  appId: 'com.myqueue.app',
+  appName: 'MyQueue Clinic',
   webDir: 'www',
   server: {
     androidScheme: 'https',
